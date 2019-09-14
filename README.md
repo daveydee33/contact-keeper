4ce6a533740e290c892b927e9a20edd4691dbd69
Dave Degeatano <davey.dee33@gmail.com>
2019-09-14 23:06:06 +0700

    Contact model + routes GET/POST to get and add contact

--- README.md
+++ README.md
@@ -45,3 +45,5 @@ curl localhost:5000/api/contacts/1 -X PUT
 - Use User model to check MongoDB if user exists, or create new instance of User model, hash the password, and save to DB.
 - Authenticate route
 - Auth middleware -- extracts the user info from the token, and add the userid to req object
+- `Contact` model.  
+  - Get contacts & Add contact (GET & POST)
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,31 +1,82 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route     GET api/contacts
 // @desc      Get all user's contacts
 // @access    Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      data: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route     POST api/contacts
 // @desc      Add a new contact
 // @access    Private
-router.post('/', (req, res) => {
-  res.send('Add a contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Name is required')
+    ]
+  ],
+  async (req, res) => {
+    // check for errors and return them
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      // I could check for a duplicate record here - but I don't think we should care
+
+      // create new contact record
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error('Error saving new contact');
+      console.error(err);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 // @route     PUT api/contacts/:id
 // @desc      Update a contact
 // @access    Private
-router.put('/:id', (req, res) => {
+router.put('/:id', auth, (req, res) => {
   res.send('Update a contact');
 });
 
 // @route     DELETE api/contacts/:id
 // @desc      Delete a contact
 // @access    Private
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, (req, res) => {
   res.send('Delete a contact');
 });
 
